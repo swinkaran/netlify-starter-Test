9fdd2da59ce46b103f9ba5a85241146156003ef7
Swinkaran <swinkaran@hotmail.com>
2023-03-22 09:06:08 +1100

    Creating contentful Api
    Creating contentful Api

---
+++ components/contentful/textWithDotPoints.js
@@ -0,0 +1,35 @@
+import { fetchGraphQL } from "lib/api";
+
+const TEXT_WITH_DOT_POINTS_CONTENT_GRAPHQL_FIELDS = `
+  name
+  heading
+  ctaText
+  ctaUrl
+  ctaVisible
+  body {
+    json
+  }
+  dotPoints {
+    json
+  }
+`;
+
+function extractTextWithDotPointsEntries(fetchResponse) {
+  return fetchResponse?.data?.textWithDotPointsCollection?.items;
+}
+
+export async function getTextWithDotPointsForHome(preview) {
+  const entries = await fetchGraphQL(
+    `query {
+      textWithDotPointsCollection(preview: ${
+        preview ? "true" : "false"
+      }, limit: 1) {
+        items {
+          ${TEXT_WITH_DOT_POINTS_CONTENT_GRAPHQL_FIELDS}
+        }
+      }
+    }`,
+    preview
+  );
+  return extractTextWithDotPointsEntries(entries);
+}
--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,7 @@
 import Head from "next/head";
 import Header from "@components/Header";
 import Footer from "@components/Footer";
+import { getTextWithDotPointsForHome } from "@components/contentful/textWithDotPoints";
 
 export default function Home() {
   return (
@@ -15,11 +16,18 @@ export default function Home() {
       <main>
         <Header title="Welcome to my app!" />
         <p className="description">
-          Get started by editing <code>pages/index.js</code>
+          Integration with Contentfull by Swinkaran!
         </p>
       </main>
 
       <Footer />
     </div>
   );
+}
+
+export async function getStaticProps({ preview = false }) {
+  const textWithDotPoints = (await getTextWithDotPointsForHome(preview)) ?? [];
+  return {
+    props: { preview, textWithDotPoints },
+  };
 }
